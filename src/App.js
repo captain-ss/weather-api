151339f8f9430661a0bae13bd92f74a15a0e11f8
captain-ss <swadeshishu2001@gmail.com>
2022-01-10 14:49:49 +0530

    final

--- src/App.js
+++ src/App.js
@@ -8,21 +8,25 @@ import "./App.css";
 function App() {
   const [city, setCity] = useState();
   const [weather, setWeather] = useState();
+  const [show, setShow] = useState(false)
 
   const fetchWeather = async (e) => {
     e.preventDefault();
     const response = await Axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=fe4feefa8543e06d4f3c66d92c61b69c`,
-      );
+      ).then(()=>{
+        setShow(true)
+      });
     console.log(response?.data);
+
   };
 
   return (
     <>
       <div className="main-app">
         <div className="app-label">React weather app</div>
-        <Citycompenonent setCity={setCity} fetchWeather={fetchWeather} />
-      </div>
+{  !show &&      <Citycompenonent setCity={setCity} fetchWeather={fetchWeather} />
+}{show && <Weathercomponent /> }  </div>
     </>
   );
 }
